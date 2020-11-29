a33b752952814491eac199cd166ec30d21be740d
Tom O'Leary <tomoleary1@Toms-MBP-92.home>
2020-11-29 12:27:13 +0000

    refactor, utils

--- vqe/vqe.c
+++ vqe/vqe.c
@@ -1,6 +1,17 @@
 
+// TODO: assuming Hamiltonian is reduced 1 term pauli entries with consts, need a way of representing this so that measurements can be decided
+
+
 #include <stdio.h>
 #include "QuEST.h"
+#include <QuEST_complex.h>
+
+/*
+* PREPARE QuEST environment
+* (Required only once per program)
+*/
+
+QuESTEnv env;
 
 /*
  *  1 -> 1
@@ -12,32 +23,13 @@ int measurementToEvalue(outcome){
     return 2*outcome - 1;
 }
 
+/* Returns a pointer to a universal gate for a single qubit
+ *
+ * Single qubit variational form : U3(theta, thi, lambda) = [[cos(theta/2), -e^(i*lambda)sin(theta/2)],[e^(i*phi)sin(theta/2),e^(i*lambda + i*phi)cos(theta/2)]] 
+ *
+ */
 
-int energyExpectation(){
-    /*
-     * PREPARE QUBIT SYSTEM
-     */
-
-    Qureg qubits = createQureg(1, env);
-    initZeroState(qubits);
-
-
-
-    /*
-     * REPORT SYSTEM AND ENVIRONMENT
-     */
-    reportQuregParams(qubits);
-    reportQuESTEnv(env);
-
-    /*
-     * ANSATZ PARAMS 
-     *
-     * Single qubit variational form : U3(theta, thi, lambda) = [[cos(theta/2), -e^(i*lambda)sin(theta/2)],[e^(i*phi)sin(theta/2),e^(i*lambda + i*phi)cos(theta/2)]] 
-     */
-    qreal theta = M_PI / 2;
-    qreal phi = 0;;
-    qreal lambda = 0;
-
+ComplexMatrix2 singleQubitVariationalForm(qreal theta, qreal phi, qreal lambda){
     qreal cosTheta = cos(theta / 2);
     qreal sinTheta = sin(theta / 2);
  
@@ -67,7 +59,17 @@ int energyExpectation(){
                            .real={{u300.real,u301.real},{u310.real,u311.real}},
                            .imag={{u300.imag,u301.imag},{u310.imag,u311.imag}}
                         };
-    
+
+    return u3;
+}
+ 
+
+int energyExpectation(qreal theta,qreal phi,qreal lambda){
+   /*
+     * ANSATZ PARAMS 
+     *
+     */
+   
     /*
      * INITIAL STATE
      */
@@ -75,9 +77,30 @@ int energyExpectation(){
     /*
      * APPLY ANSATZ GATES
      */ 
-    unitary(qubits,0,u3);
-    Qureg qubitsX = createQureg(qubits, env); //copy circuit
-    Qureg qubitsY = createQureg(qubits, env); //copy circuit
+    ComplexMatrix2 u3 = singleQubitVariationalForm(theta, phi, lambda);
+ 
+     /*
+     * PREPARE QUBIT SYSTEM
+     */
+
+    Qureg qubitsZ = createQureg(1, env);
+    
+    initZeroState(qubitsZ);
+    unitary(qubitsZ,0,u3);
+   
+    Qureg qubitsX = createQureg(1, env);
+    cloneQureg(qubitsX,qubitsZ); //copy circuit
+    Qureg qubitsY = createQureg(1, env);
+    cloneQureg(qubitsY, qubitsZ); //copy circuit
+ 
+     /*
+     * REPORT SYSTEM AND ENVIRONMENT
+     */
+    reportQuregParams(qubitsZ);
+    reportQuregParams(qubitsX);
+    reportQuregParams(qubitsY);
+    reportQuESTEnv(env);
+
     /*
      * MEASUREMENTS : MEASURE EACH SUB HAMILTONIAN 
      */
@@ -85,26 +108,26 @@ int energyExpectation(){
     /*
      * PAULI Z MEASUREMENTS : JUST MEASURE IN COMPUTATIONAL BASIS
      */
-    int outcome = measure(qubits, 0);
-    printf("Qubit 0 was measured in state %d\n", outcome);
-    printf("Qubit 0 output measurement as pauli z eigenvalue: %d\n", measurementToEvalue(outcome));
+    int outcomeZ = measure(qubitsZ, 0);
+    printf("Qubit 0 was measured in state %d\n", outcomeZ);
+    printf("Qubit 0 output measurement as pauli z eigenvalue: %d\n", measurementToEvalue(outcomeZ));
 
     /*
      * PAULI X MEASUREMENTS : ROTATE BASIS ABOUT Y AXIS BY -PI/2, MEASURE IN COMPUTATIONAL BASIS
      */
     rotateY(qubitsX,0,-1*M_PI/2);  
-    int outcome = measure(qubitsX, 0);
-    printf("Qubit 0 was measured in state %d\n", outcome);
-    printf("Qubit 0 output measurement as pauli x eigenvalue: %d\n", measurementToEvalue(outcome));
+    int outcomeX = measure(qubitsX, 0);
+    printf("Qubit 0 was measured in state %d\n", outcomeX);
+    printf("Qubit 0 output measurement as pauli x eigenvalue: %d\n", measurementToEvalue(outcomeX));
 
 
     /*
      * PAULI Y MEASUREMENTS : ROTATE BASIS ABOUT Y AXIS BY PI/2, MEASURE IN COMPUTATIONAL BASIS
      */
     rotateY(qubitsY,0,M_PI/2);  
-    int outcome = measure(qubitsY, 0);
-    printf("Qubit 0 was measured in state %d\n", outcome);
-    printf("Qubit 0 output measurement as pauli y eigenvalue: %d\n", measurementToEvalue(outcome));
+    int outcomeY = measure(qubitsY, 0);
+    printf("Qubit 0 was measured in state %d\n", outcomeZ);
+    printf("Qubit 0 output measurement as pauli y eigenvalue: %d\n", measurementToEvalue(outcomeZ));
 
 
     
@@ -126,7 +149,7 @@ int energyExpectation(){
      * FREE MEMORY
      */
 
-    destroyQureg(qubits, env); 
+    destroyQureg(qubitsY, env); 
     destroyQureg(qubitsX, env); 
     destroyQureg(qubitsZ, env); 
 
@@ -140,27 +163,36 @@ int energyExpectation(){
 
 }
 
+/* Performs gradient descent with params for 1 qubit variational form
+ * 
+ * TODO: If possible, make this modular & generic for any function and a list of params
+ */
+
 int gradientDescent(){
-    int cost = energyExpectation();
+    qreal theta = M_PI / 2;
+    qreal phi = 0;
+    qreal lambda = 0;
+
+
+    int cost = energyExpectation(theta, phi, lambda);
     
     return cost;
 }
 int main (int narg, char *varg[]) {
 
-    
+    env = createQuESTEnv();
 
-    /*
-     * PREPARE QuEST environment
-     * (Required only once per program)
-     */
 
-    QuESTEnv env = createQuESTEnv();
 
     printf("-------------------------------------------------------\n");
     printf("WOOOOOOOO its vqe:\n\t baby.\n");
     printf("-------------------------------------------------------\n");
 
     int groundStateEnergyBound = gradientDescent();
+    
+
+      
+    printf("Ground state energy bound %d \n",groundStateEnergyBound);
     /*
      * CLOSE QUEST ENVIRONMET
      * (Required once at end of program)
